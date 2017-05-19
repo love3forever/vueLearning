cf68636e1d4e72dbf591757f555422e7951b65a3
wangmengcn <eclipse_sv@163.com>
2017-05-19 17:03:07 +0800

    test added

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-05-19 17:02:22
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import this
